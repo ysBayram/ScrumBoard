e39e000994b72f803ce847b4e37cca72e0876c56
Murat Başeren <muratbaseren@gmail.com>
2015-06-01 17:02:41 +0300

    ucMain kontrolüne UserName eklendi.

--- ScrumBoard/UserControls/ucMain.Designer.cs
+++ ScrumBoard/UserControls/ucMain.Designer.cs
@@ -29,20 +29,22 @@
         private void InitializeComponent()
         {
             this.pnlTask = new MetroFramework.Controls.MetroPanel();
-            this.pnlPriority = new System.Windows.Forms.Panel();
+            this.pbUser = new System.Windows.Forms.PictureBox();
             this.lblDescrp = new MetroFramework.Controls.MetroLabel();
             this.lblTime = new MetroFramework.Controls.MetroLabel();
-            this.pbUser = new System.Windows.Forms.PictureBox();
+            this.pnlPriority = new System.Windows.Forms.Panel();
             this.lblName = new System.Windows.Forms.Label();
             this.toolTipUser = new MetroFramework.Components.MetroToolTip();
+            this.lblUserName = new MetroFramework.Controls.MetroLabel();
             this.pnlTask.SuspendLayout();
-            this.pnlPriority.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbUser)).BeginInit();
+            this.pnlPriority.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlTask
             // 
             this.pnlTask.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlTask.Controls.Add(this.lblUserName);
             this.pnlTask.Controls.Add(this.pbUser);
             this.pnlTask.Controls.Add(this.lblDescrp);
             this.pnlTask.Controls.Add(this.lblTime);
@@ -59,34 +61,6 @@
             this.pnlTask.VerticalScrollbarHighlightOnWheel = false;
             this.pnlTask.VerticalScrollbarSize = 10;
             // 
-            // pnlPriority
-            // 
-            this.pnlPriority.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.pnlPriority.Controls.Add(this.lblName);
-            this.pnlPriority.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlPriority.Location = new System.Drawing.Point(0, 0);
-            this.pnlPriority.Name = "pnlPriority";
-            this.pnlPriority.Size = new System.Drawing.Size(175, 23);
-            this.pnlPriority.TabIndex = 2;
-            // 
-            // lblDescrp
-            // 
-            this.lblDescrp.Location = new System.Drawing.Point(3, 2);
-            this.lblDescrp.Name = "lblDescrp";
-            this.lblDescrp.Size = new System.Drawing.Size(167, 111);
-            this.lblDescrp.TabIndex = 2;
-            this.lblDescrp.Text = "Description";
-            // 
-            // lblTime
-            // 
-            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.lblTime.AutoSize = true;
-            this.lblTime.Location = new System.Drawing.Point(7, 109);
-            this.lblTime.Name = "lblTime";
-            this.lblTime.Size = new System.Drawing.Size(96, 19);
-            this.lblTime.TabIndex = 3;
-            this.lblTime.Text = "01.01.2001 01:01";
-            // 
             // pbUser
             // 
             this.pbUser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
@@ -97,6 +71,34 @@
             this.pbUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbUser.TabIndex = 4;
             this.pbUser.TabStop = false;
+            // 
+            // lblDescrp
+            // 
+            this.lblDescrp.Location = new System.Drawing.Point(3, 2);
+            this.lblDescrp.Name = "lblDescrp";
+            this.lblDescrp.Size = new System.Drawing.Size(167, 91);
+            this.lblDescrp.TabIndex = 2;
+            this.lblDescrp.Text = "Description";
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblTime.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.lblTime.Location = new System.Drawing.Point(7, 114);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(126, 14);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "01.01.2001 01:01";
+            // 
+            // pnlPriority
+            // 
+            this.pnlPriority.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.pnlPriority.Controls.Add(this.lblName);
+            this.pnlPriority.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlPriority.Location = new System.Drawing.Point(0, 0);
+            this.pnlPriority.Name = "pnlPriority";
+            this.pnlPriority.Size = new System.Drawing.Size(175, 23);
+            this.pnlPriority.TabIndex = 2;
             // 
             // lblName
             // 
@@ -113,6 +115,17 @@
             this.toolTipUser.StyleManager = null;
             this.toolTipUser.Theme = MetroFramework.MetroThemeStyle.Dark;
             // 
+            // lblUserName
+            // 
+            this.lblUserName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblUserName.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.lblUserName.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.lblUserName.Location = new System.Drawing.Point(7, 96);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(126, 15);
+            this.lblUserName.TabIndex = 5;
+            this.lblUserName.Text = "[UserName]";
+            // 
             // ucMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -123,10 +136,9 @@
             this.Name = "ucMain";
             this.Size = new System.Drawing.Size(175, 155);
             this.pnlTask.ResumeLayout(false);
-            this.pnlTask.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUser)).EndInit();
             this.pnlPriority.ResumeLayout(false);
             this.pnlPriority.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbUser)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -140,5 +152,6 @@
         private MetroFramework.Controls.MetroLabel lblTime;
         private System.Windows.Forms.Label lblName;
         private MetroFramework.Components.MetroToolTip toolTipUser;
+        private MetroFramework.Controls.MetroLabel lblUserName;
     }
 }
